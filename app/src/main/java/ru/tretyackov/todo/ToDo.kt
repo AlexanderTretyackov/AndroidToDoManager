efbef90e78165451c1fa9be26e16b2f831f0dbbf
Alexander Tretyackov <tretyackov@biogenom.ru>
2024-02-15 10:30:58 +0500

    feat: Приоритет

--- app/src/main/java/ru/tretyackov/todo/ToDo.kt
+++ app/src/main/java/ru/tretyackov/todo/ToDo.kt
@@ -4,5 +4,8 @@ import java.util.Calendar
 import java.util.Date
 import java.util.UUID
 
+enum class ToDoPriority{
+    No,Low,High
+}
 data class ToDo(var name: String, var completed:Boolean, val id:String = UUID.randomUUID().toString(),
-                val createdAt: Date = Calendar.getInstance().time)
+                val createdAt: Date = Calendar.getInstance().time, var priority: ToDoPriority = ToDoPriority.No)
