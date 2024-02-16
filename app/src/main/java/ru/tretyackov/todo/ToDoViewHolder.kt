942f5e3abedc6f5f06e46481e8e8a3d9e599eebf
Alexander Tretyackov <tretyackov@biogenom.ru>
2024-02-16 10:16:27 +0500

    feat: Дедлайн

--- app/src/main/java/ru/tretyackov/todo/ToDoViewHolder.kt
+++ app/src/main/java/ru/tretyackov/todo/ToDoViewHolder.kt
@@ -23,6 +23,7 @@ class ToDoViewHolder(itemView: View, private val onClick : (ToDo)->Unit,
     private val textView = itemView.findViewById<TextView>(R.id.toDoNameTextView)
     private val checkBox = itemView.findViewById<CheckBox>(R.id.checkBoxCompleted)
     private val priorityImageView = itemView.findViewById<ImageView>(R.id.priorityImageView)
+    private val itemDeadlineTextView = itemView.findViewById<TextView>(R.id.itemDeadlineTextView)
     fun onBind(todo:ToDo){
         textView.text = todo.name
         checkBox.isChecked = todo.completed
@@ -33,6 +34,8 @@ class ToDoViewHolder(itemView: View, private val onClick : (ToDo)->Unit,
             ToDoPriority.Low ->  priorityImageView.setImageResource(R.drawable.low)
             ToDoPriority.High ->  priorityImageView.setImageResource(R.drawable.high)
         }
+        itemDeadlineTextView.visibility = if(todo.deadline != null) View.VISIBLE else View.GONE
+        itemDeadlineTextView.text = todo.deadline?.toFormattedString()
         checkBox.setOnClickListener{
             todo.completed = !todo.completed
             textView.updateCompleteStyle(todo.completed)
