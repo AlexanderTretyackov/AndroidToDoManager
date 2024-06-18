d84c054650fe0f7fcb048c5dfb4d7d00c62e4c99
Alexander Tretyackov <tretyackov@biogenom.ru>
2024-06-18 20:34:13 +0500

    feat: Более репрезентативное наполнение задач

---
+++ app/src/main/java/ru/tretyackov/todo/DateHelper.kt
@@ -0,0 +1,13 @@
+package ru.tretyackov.todo
+
+import java.util.Calendar
+import java.util.Date
+
+object DateHelper {
+    fun dateFromYearMonthDay(year:Int,month:Int,day:Int): Date
+    {
+        val calendar: Calendar = Calendar.getInstance()
+        calendar.set(year,month,day)
+        return calendar.time
+    }
+}
