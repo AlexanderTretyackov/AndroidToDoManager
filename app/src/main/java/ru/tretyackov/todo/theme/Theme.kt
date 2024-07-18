aa67d9642c4fb69625caf637c6d059fcbbf9822c
Alexander Tretyackov <tretyackov.aleks2012@yandex.ru>
2024-07-18 18:55:15 +0500

    feat: Более совершенная стилизация и выбор темы

--- app/src/main/java/ru/tretyackov/todo/theme/Theme.kt
+++ app/src/main/java/ru/tretyackov/todo/theme/Theme.kt
@@ -3,20 +3,40 @@ package ru.tretyackov.todo.theme
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProvideTextStyle
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.alpha
+import ru.tretyackov.todo.R
 
 @Composable
 fun AppTheme(content: @Composable () -> Unit) {
-    val textColor = if(isSystemInDarkTheme()) whiteColor else blackColor
-    MaterialTheme(colorScheme = if(isSystemInDarkTheme()){
-        darkColorScheme(primary = blueColor)
-    } else lightColorScheme(primary = blueColor), content = {
-        ProvideTextStyle(
-            value = TextStyle(color = textColor),
-            content = content
-        )
-    })
+    val customTypography = Typography(
+        titleLarge = TextStyle(fontSize = 32.sp),
+        titleMedium = TextStyle(fontSize = 20.sp),
+        bodyMedium = TextStyle(fontSize = 16.sp),
+        bodySmall = TextStyle(fontSize = 14.sp),
+    )
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) {
+            darkColorScheme(
+                primary = primaryColor,
+                onPrimary = blueColor,
+            )
+        } else lightColorScheme(
+            primary = blueColor,
+            onPrimary = blueColor,
+        ), content = {
+            ProvideTextStyle(
+                value = TextStyle(color = colorResource(id = R.color.defaultTextColor)),
+                content = content
+            )
+        },
+        typography = customTypography
+    )
 }
